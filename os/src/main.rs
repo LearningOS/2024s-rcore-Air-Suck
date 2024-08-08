c193d6b68fcc9d2fd2597a0af34e5a980569f1f9
snh <2640327187@qq.com>
2024-08-08 15:51:03 +0800

    test ch4

--- os/src/main.rs
+++ os/src/main.rs
@@ -73,7 +73,7 @@ fn kernel_log_info() {
         fn boot_stack_top(); // stack top
     }
     logging::init();
-    println!("[kernel] Hello, world!");
+    println!("[kernel] Hello, world! I am here");
     trace!(
         "[kernel] .text [{:#x}, {:#x})",
         stext as usize,
